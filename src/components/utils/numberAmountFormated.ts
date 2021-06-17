44f10843be9ef7c4a8be8dc675b020cda6405215
Nasalis <davidossantosfreitas@hotmail.com>
2021-06-16 22:42:43 -0300

    new rule to amount's formating

--- src/components/utils/numberAmountFormated.ts
+++ src/components/utils/numberAmountFormated.ts
@@ -2,16 +2,20 @@ export const amountAnything = {
 
     formatNumber(amount: number) {
 
+        const symbols = ["","","mi","bi"]
+
         const tier = Math.log10(amount) / 3 | 0;
         
         if(tier === 0)
             return amount;
+
+        const sufix = symbols[tier];
         
         const scale = Math.pow(10, tier*3);
         
         const scaled = amount/scale;
 
-        return scaled;
+        return ((amount > 999999) ? scaled.toFixed(3) : scaled) + " " + sufix;
     },
 
     fansAmountMessage(fans: number): string {
