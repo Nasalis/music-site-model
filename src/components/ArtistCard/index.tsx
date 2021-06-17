e7cdab8890af1768a819428391e944052165bbf1
Nasalis <davidossantosfreitas@hotmail.com>
2021-06-16 22:47:59 -0300

    amount fans message modified

--- src/components/ArtistCard/index.tsx
+++ src/components/ArtistCard/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useTheme } from '../ThemeContext';
+import Link from 'next/link';
 import styles from './style.module.scss';
-
+import { amountAnything } from '../utils/numberAmountFormated';
 interface Artist {
     name: string,
     nb_album: number,
     nb_fan: number,
     picture_medium: string,
+    id: number;
 }
 
 interface ArtistCardProps {
@@ -18,20 +20,22 @@ export function ArtistCard({artist}: ArtistCardProps) {
     const {theme} = useTheme();
 
     return (
-        <div className={styles.artistCardContainer}>
-            <div className={styles.header}>
-                <img src={artist.picture_medium} alt={artist.name}/>
-                <i className="fas fa-heart"></i>
+        <Link href={`/artist/${artist.id}`}>
+            <div className={styles.artistCardContainer}>
+                <div className={styles.header}>
+                    <img src={artist.picture_medium} alt={artist.name}/>
+                    <i className="fas fa-heart"></i>
+                </div>
+                <h4 style={{
+                    color: theme.colors.text
+                }} >{artist.name}</h4>
+                <div className={styles.cardInformations}>
+                    <span style={{color: theme.colors.text}}>Albuns: {artist.nb_album}</span>
+                    <span style={{color: theme.colors.text}}>
+                        {amountAnything.fansAmountMessage(artist.nb_fan)}
+                    </span>
+                </div>
             </div>
-            <h4 style={{
-                color: theme.colors.text
-            }} >{artist.name}</h4>
-            <div className={styles.cardInformations}>
-                <span style={{color: theme.colors.text}}>Albuns: {artist.nb_album}</span>
-                <span style={{color: theme.colors.text}}>{artist.nb_fan}
-                    <strong style={{color: theme.colors.text}}>fans</strong>
-                </span>
-            </div>
-        </div>
+        </Link>
     )
 }
