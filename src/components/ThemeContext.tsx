a94b36e8854398ca3cbd67c7c60b1a4c3b892fc3
Nasalis <davidossantosfreitas@hotmail.com>
2021-06-18 15:07:13 -0300

    ThemeContext code updated

--- src/components/ThemeContext.tsx
+++ src/components/ThemeContext.tsx
@@ -16,9 +16,8 @@ interface Theme {
 }
 
 interface TableContextData {
-    theme: Theme,
-    themes: Array<Theme>
-    setTheme: (state: Theme) => void,
+    darkMode: boolean;
+    setDarkMode: (darkmode: boolean) => void;
 }
 
 interface TableProviderProps {
@@ -27,51 +26,16 @@ interface TableProviderProps {
 
 export const ThemeContext = createContext({} as TableContextData);
 
-export const themes = [
-    {
-        title : 'light',
-        colors: {
-            background: '#f5f5f5',
-            backgroundBold: '#f7f7f7',
-            text: '#333',
-            menubarColor: '#131313',
-            artistCardColor: '#252525',
-            playlistOrAlbumColor: '#72727d',
-            priceCardColorBackground: '#f8f9fa',
-            priceCardColorBackgroundFooter: '#ebe6e6',
-            grayColor4:'#4e4c4c',
-            
-        }
-    },
-    
-    {
-        title : 'dark',
-        colors: {
-            background: '#222',
-            backgroundBold: '#141414',
-            text: '#fff',
-            menubarColor: '#f2f2f2',
-            artistCardColor: '#e9e6e6',
-            playlistOrAlbumColor: '#cbcbce',
-            priceCardColorBackground: '#212122',
-            priceCardColorBackgroundFooter: '#2e2b2b',
-            grayColor4:'#b4b3b3',
-        }
-    }
-]
-
 export function ThemeContextProvider({children}: TableProviderProps) {
-    const [theme, setTheme] = useState(themes[0]);
+    const [darkMode, setDarkMode] = useState(false);
 
     return (
-        <ThemeContext.Provider value={{theme, themes, setTheme}}>
+        <ThemeContext.Provider value={{darkMode, setDarkMode}}>
             {children}
         </ThemeContext.Provider>
     )
 }
 
 export function useTheme() {
-    const context = useContext(ThemeContext);
-    const {theme, themes, setTheme} = context;
-    return {theme, themes, setTheme}
+    return useContext(ThemeContext);
 }
