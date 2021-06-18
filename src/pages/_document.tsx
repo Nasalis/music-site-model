da74211ba744fe0e1c18f0b9a61549dcd2e05ebb
Nasalis <davidossantosfreitas@hotmail.com>
2021-06-18 15:07:53 -0300

    HTML's lang atributte added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,8 @@ import Document, {Html, Head, Main, NextScript} from 'next/document';
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="pt-BR">
+                <title>Wave - Site</title>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,600;0,700;1,500&display=swap" rel="stylesheet"/>
