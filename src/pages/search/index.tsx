b464ad31a343a59f73de0c6f6ce8a9ab618f4deb
Davi dos Santos <davidossantosfreitas@hotmail.com>
2021-08-21 16:33:03 -0300

    Alert to no founded item added and search changed

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -17,55 +17,73 @@ export default function Search() {
 
     const [itemToSearch, setItemToSearch] = useState("");
     const [foundItem, setFoundItem] = useState<Search>({} as Search);
+    const [itemHasFounded, setItemHasFounded] = useState(false);
 
-    useEffect(() => {
-        async function searchContent() {
-            const data = await fetch(`${apiInfo.BASE_URL}/search?q=${itemToSearch.toLowerCase()}`, {
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-key": apiInfo.API_KEY,
-                    "x-rapidapi-host": apiInfo.HOST
-                }
-            })
-            .then(response => response.json())
-            setFoundItem(data);
+    function emptyItemToSearch() {
+        return setItemToSearch("");
+    }
+
+    async function searchContent() {
+        if(itemToSearch === "") {
+            setItemHasFounded(false);
+            return;
         }
-        searchContent();
-    },[itemToSearch])
+        const data = await fetch(`${apiInfo.BASE_URL}/search?q=${itemToSearch.toLowerCase()}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": apiInfo.API_KEY,
+                "x-rapidapi-host": apiInfo.HOST
+            }
+        })
+        .then(response => response.json())
+        setFoundItem(data);
+        setItemHasFounded(true);
+    }
     
     console.log(itemToSearch)
     console.log(foundItem)
 
     return (
         <div className={style.searchContainer}>
-            <input onChange={(event) => setItemToSearch(event.target.value)}/>
-            <button type="button">APERTAR</button>
-            <p>Total Itens: {foundItem.total}</p>
+            <div className={style.inputForTracks}>
+                <input onChange={(event) => setItemToSearch(event.target.value)}/>
+                <button type="button" onClick={searchContent}>Search</button>
+                <p>Total Itens: {foundItem.total}</p>
+            </div>
 
-            <div className={style.itemsContainer}>
-                {foundItem.data?.map((item: Track) => (
-                    <div key={item.id} className={style.trackContainer}>
-                        <div className={style.imageTrack}>
-                            <img src={item.album.cover_medium} alt="" />
-                        </div>
-                        <div className={style.infoBarTrack}>
-                            <div className={style.trackTitle}>
-                                <h3 title={item.title_short}>{item.title_short}</h3>
-                                <div>
-                                    <span>by {item.artist.name}</span>
-                                    <img src={item.artist.picture_small}/>
+            {(!itemHasFounded || foundItem.total === 0)? (
+                <div className={style.alertBox}>
+                    <h2>Any track has been founded</h2>
+                    <small>Try typing again in input</small>
+                    <i className="fas fa-database"></i>
+                </div>
+            ) : (
+                <div className={style.itemsContainer}>
+                    {foundItem.data?.map((item: Track) => (
+                        <div key={item.id} className={style.trackContainer} >
+                            <div className={style.imageTrack}>
+                                <img src={item.album.cover_medium} alt="" />
+                            </div>
+                            <div className={style.infoBarTrack}>
+                                <div className={style.trackTitle}>
+                                    <h3 title={item.title_short}>{item.title_short}</h3>
+                                    <div>
+                                        <span>by {item.artist.name}</span>
+                                        <img src={item.artist.picture_small}/>
+                                    </div>
+                                </div>
+                                <div className={style.infoFromTrack}>
+                                    {item.explicit_lyrics && (
+                                        <span className={style.explicit}>Explicit</span>
+                                    )}
+                                    <span>Duration: {convertSecondsToMinutes(item.duration)}</span>
                                 </div>
                             </div>
-                            <div className={style.infoFromTrack}>
-                                {item.explicit_lyrics && (
-                                    <span className={style.explicit}>Explicit</span>
-                                )}
-                                <span>Duration: {convertSecondsToMinutes(item.duration)}</span>
-                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
+
             
         </div>
     )
